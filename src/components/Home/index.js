8d3940c852aaa2de94a35053cfd9cd871c8528f4
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-20 15:51:25 -0700

    implemented animated effects for my home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
@@ -7,10 +7,21 @@ const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['A','n','u','s','h','a'];
     const jobArray = ['W','e','b',' ','d','e','v','e','l','o','p','e','r'];
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm <AnimatedLetters letterClass={letterClass}
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass}_12`}>i,</span>
+                     <br /> 
+                    <span className={`${letterClass}_13`}>I</span>
+                    <span className={`${letterClass}_14`}>'m</span> <AnimatedLetters letterClass={letterClass}
                 strArray={nameArray}
                 idx={15}/> <br /> <AnimatedLetters letterClass={letterClass}
                 strArray={jobArray}
