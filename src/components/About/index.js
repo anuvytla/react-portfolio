16ea29b75c3bc5bcdfcb3dd0ae4de00da09f01c1
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-21 15:12:58 -0700

    fixed the bugs and animated the about page text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,25 +1,34 @@
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
     return(
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
-                    strArray={['A','b','o','u','t',' ', 'm','e']}
-                    idx={15}
-                     />
+                        letterClass={letterClass}
+                        strArray={['A','b','o','u','t',' ', 'm','e']}
+                        idx={15}
+                    />
                 </h1>
                 <p>
-                Hi This is Anusha!. I'm experienced Software Engineer in a top notch companies like Verizon and Thomson Reuters. Now, i decided to  expand my knowledge and build my experience as a full stack developer in accordance to that  i have done my full stack cource in UC Berkely EXTN bootcamp.
-                As an enthusiastic and creative technologist with hands on skills in wide range of tools and programming languages. I am well positioned to leverage my abilities and qualifications as a Full stack MERN developer.
-                Throughout my career, I have been acknowledged as a solution-focussed professional with the ability to deliver clean solutions, design and develop innovatiave applications using advanced technologies.
-                In my previous employement , I have consistenly met client expectations and project milestoneswhile working effectively in deadline driven environment with clean deliverables.
-                <br></br>
-                Few insights of me as full stack MERN developer
-                <br></br>
-                <br></br>
+                    Hi This is Anusha!. I'm experienced Software Engineer in a top notch companies like Verizon and Thomson Reuters. Now, i decided to  expand my knowledge and build my experience as a full stack developer in accordance to that  i have done my full stack cource in UC Berkely EXTN bootcamp.
+                    As an enthusiastic and creative technologist with hands on skills in wide range of tools and programming languages. I am well positioned to leverage my abilities and qualifications as a Full stack MERN developer.
+                    Throughout my career, I have been acknowledged as a solution-focussed professional with the ability to deliver clean solutions, design and develop innovatiave applications using advanced technologies.
+                    In my previous employement , I have consistenly met client expectations and project milestoneswhile working effectively in deadline driven environment with clean deliverables.
+                </p>
+                <br/>
+                <p>Few insights of me as full stack MERN developer</p>
+                <br/>
                 <ul>
                     <li>Designed and developed front-end   webpages using HTML5, JQUERY, Vanilla JavaScript (ES6), CSS, BOOTSTRAP,MDN BOOTSTRAP, REACT, REACT BOOTSTRAP.</li>
             
@@ -28,10 +37,9 @@ const About = () => {
                     <li>Created many ecommerce backend sites  where you can find them in my Github </li>
                 </ul>
             
-                <br></br>
-                I would welcome the chance to provide further insight in to my technical abilities and personal attributes 
-                </p>
-            </div>
+                <br/>
+                <p>I would welcome the chance to provide further insight in to my technical abilities and personal attributes</p> 
+            </div> 
         </div>
     )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,10 +9,10 @@ const Home = () => {
     const jobArray = ['W','e','b',' ','d','e','v','e','l','o','p','e','r'];
 
     useEffect(() => {
-        return setTimeout(() => {
+        setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 4000)
-    }, [])
+    }, []);
     return(
         <div className="container home-page">
             <div className="text-zone">
@@ -21,11 +21,15 @@ const Home = () => {
                     <span className={`${letterClass}_12`}>i,</span>
                      <br /> 
                     <span className={`${letterClass}_13`}>I</span>
-                    <span className={`${letterClass}_14`}>'m</span> <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}/> <br /> <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22}/></h1>
+                    <span className={`${letterClass}_14`}>'m</span> 
+                    <AnimatedLetters letterClass={letterClass}
+                                     strArray={nameArray}
+                                     idx={15}/> 
+                    <br /> 
+                    <AnimatedLetters letterClass={letterClass}
+                                     strArray={jobArray}
+                                     idx={22}/>
+                    </h1>
                 
                 <h2>Fullstack Developer / Javascript Expert / Python Expert</h2>
                 <Link to='/contact' className='flat-button'>CONATCT ME</Link>
