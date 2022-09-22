9e1bcfc3795f530b8c049d4b6d01dfe4657055b0
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-21 18:26:40 -0700

    add contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import Loader from 'react-loaders';
+import './index.scss';
+
+
+const Contact = () => {
+    return(
+        <>
+            <div>
+
+            </div>
+            <Loader type='pacman'/>
+        </>
+    )
+}
+
+
+export default Contact;
