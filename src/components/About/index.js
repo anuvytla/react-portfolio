929867668f273610def5f50a1a8a65f0b3514a46
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-21 17:11:14 -0700

    add cube spinner animation with technologies

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,11 @@
+import { faAngular,
+         faCss3, 
+         faGithub, 
+         faHtml5, 
+         faJsSquare, 
+         faReact }
+          from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
@@ -39,6 +47,39 @@ const About = () => {
             
                 <br/>
                 <p>I would welcome the chance to provide further insight in to my technical abilities and personal attributes</p> 
+            </div>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faAngular} color="DD0031"/>
+                    </div>
+
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="F06529"/>
+                    </div>
+
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color="28A4D9"/>
+                    </div>
+
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faReact} color="5ED4F4"/>
+                    </div>
+
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJsSquare} color="EFD81D"/>
+                    </div>
+
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGithub} color="EC4D28"/>
+                    </div>
+
+
+
+
+
+
+                </div>
             </div> 
         </div>
     )
