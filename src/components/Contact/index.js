6e218aa9212ee2518f52d708bf3ddb3cbe509789
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-21 18:42:07 -0700

    add animated letters for contact me with react hooks

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,29 @@
-import Loader from 'react-loaders';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 
 const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
     return(
         <>
-            <div>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                                letterClass={letterClass}
+                                strArray={['C','o','n','t','a','c','t',' ','m','e']}
+                                idx={15} 
+                        />
+                    </h1>
+
+                </div>
 
             </div>
             <Loader type='pacman'/>
