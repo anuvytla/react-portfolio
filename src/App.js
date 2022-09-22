97f7a553cc267226b61049fc0074273246a6ca37
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-22 16:59:45 -0700

    create routes to contact resume and portfolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import About from './components/About'
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
 function App() {
   return (
