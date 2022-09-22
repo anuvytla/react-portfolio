436103922a9fea3341697f465792c3a127e42559
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-22 16:56:57 -0700

    create portfolio components and animated the heading

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import Loader from 'react-loaders';
 
-const Portfolio = () => ('Hello')
+const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
+    return(
+        <>
+            <div className='container portfolio-page'>
+                <div className='text-zone'>
+                <h1>
+                        <AnimatedLetters
+                                letterClass={letterClass}
+                                strArray={['P','o','r','t','f','o','l','i','o']}
+                                idx={15} 
+                        />
+                    </h1>
+
+                <div className='projects'></div>
+                </div>
+            
+            </div>
+            <Loader type='pacman'/>
+        </>
+    )
+}
 
 export default Portfolio;
