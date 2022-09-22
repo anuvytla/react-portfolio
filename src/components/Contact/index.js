2dc1e135c7f1f638b84c52f52870db1579f8794c
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-21 22:16:43 -0700

    add contact form with  name email subject and submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import Loader from 'react-loaders';
 
 
 const Contact = () => {
@@ -22,6 +23,32 @@ const Contact = () => {
                                 idx={15} 
                         />
                     </h1>
+                    <p>
+                        I am open for freelancing project for details contact me via upwork.
+                        Experties in Javascript,Jquery,HTML5,CSS, React,Node,Express,MongoDB,SQL,GraphQL,React Dev Tools,JWT,
+                    </p>
+
+                    <div className='contact-form'>
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name="email" placeholder='Email' required />
+                                </li>
+                                <li className='half'>
+                                    <input type="text" name="subject" placeholder='Subject' required />
+                                </li>
+                                <li className='half'>
+                                    <textarea name="message" placeholder='Message' required />
+                                </li>
+                                <li >
+                                    <input type="submit" className='flat-button' value='SEND' required />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
 
                 </div>
 
