4c469a8563f933aa9af0b976758c5b7d1db6407b
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-26 19:21:02 -0700

    modifed name header  with animated letters styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,34 @@ import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faFile, faTasks, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 
 
 const Header = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
     return (
         <div className='nav-bar'>
-            <Link className='logo' to='/'>
-                <h1>LAV</h1>
-            </Link>
+            <div className='text-zone'>
+                <Link className='logo' to='/'>
+                    <h1>
+                        <AnimatedLetters
+                                letterClass={letterClass}
+                                strArray={['L','A','V']}
+                                idx={15} 
+                        />
+                    </h1>
+            
+                
+                </Link>
+            </div>
             <nav>
                 <NavLink exact='true' activeclassname='active' to='/'>
                     <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
