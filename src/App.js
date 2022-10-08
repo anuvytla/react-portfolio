767869e251ee5c655b15372fce36e262a98c5de1
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-10-07 20:57:10 -0700

    Add gh-pages

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Layout />} >
-      <Route index element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/portfolio' element={<Portfolio />} />
-      <Route path='/resume' element={<Resume />} />
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/resume' element={<Resume />} />
       </Route>
     </Routes>
     </>
